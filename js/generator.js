5b4598714864c224bdbc114e9145f17c2de3e3b8
Daniel Ajilore <mtme13@hotmail.com>
2018-08-18 19:31:27 +0100

    remove prevent default

--- js/generator.js
+++ js/generator.js
@@ -1,15 +1,11 @@
-//http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en - json
-//https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=parseQuote&lang=en - jsonp
-
 $(document).ready(function(){
 
 var quote;
 var author;
-//declare global variables
 
   function getTheQuote(){
     $.ajax({
-      url: "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=parseQuote&lang=en",
+      url: "https://api.forismatic.com/api/1.0/",
       jsonp: "jsonp",
       dataType: "jsonp",
       data:{
@@ -17,43 +13,38 @@ var author;
         lang: "en",
         format: "jsonp"
       },
-//using $.ajax method, fetch quote & author data from the Forismatic API
-
       success: function(data) {
         quote = data.quoteText;
         author = data.quoteAuthor;
-//store fetched data in previously declared global variables
 
-        $(".quote").html(quote);
-//loads an initial quote when page loads
+        $(".quote").html(quote); 
 
+        // check if author is provided
         if(author){
           $(".author").html("- " + author);
           }
         else{
             $(".author").html("- Unknown")
             }
-//set default author & replacement value if author data not provided
         }
       });
     };
 
-getTheQuote();
-//call function to load initial quote
+getTheQuote(); //call function to load initial quote
 
-$(".new_post").click(function(){
+$(".new_post").click(function(event){
   event.preventDefault();
   getTheQuote();
 });
 //call function when new quote button is clicked
 
 function tweetQuote(){
-    event.preventDefault();
+    // event.preventDefault();
     window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent(quote + " ---  " + author));
 }
 
 $(".tweet").click(function(){
-  event.preventDefault();
+  // event.preventDefault();
   tweetQuote();
 });
 
